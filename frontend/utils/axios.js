8826006188d1d611124472f6180fe6ff9454c010
Nirupam <72504541+nannurinirupamreddy@users.noreply.github.com>
2026-03-22 17:15:36 -0400

    Update axios.js

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://snip-backend-d8yy.onrender.com',
 });
 
 export default instance;
